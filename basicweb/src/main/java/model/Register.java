58d1ba9c83f8f99be870c46a550b2c81b82a4f6c
Dharshan21k <dharsankumar160@gmail.com>
2025-12-16 19:34:50 +0530

    Create Register.java

---
+++ basicweb/src/main/java/model/Register.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Register {
+	String userName;
+	String email;
+	String password;
+	
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+
+}
